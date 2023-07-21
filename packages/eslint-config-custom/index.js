3fb45a9ce5798670051be00d15c9c74e80bc9d70
julio4 <jules.doumeche@gmail.com>
2023-07-21 19:47:57 +0200

    chore: monorepo setup

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,9 +1,22 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:snarkyjs/recommended",
+    "turbo",
+    "next",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint", "snarkyjs"],
   rules: {
+    "no-constant-condition": "off",
+    "prefer-const": "off",
     "@next/next/no-html-link-for-pages": "off",
   },
+  parser: "@typescript-eslint/parser",
   parserOptions: {
+    ecmaVersion: "latest",
     babelOptions: {
       presets: [require.resolve("next/babel")],
     },
