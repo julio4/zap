81c99ff27a2f2638250461cdd4403dfb91eaf4e9
julio4 <jules.doumeche@gmail.com>
2023-07-23 04:19:47 +0200

    feat(ui): landing page, steps ux flow

--- apps/web/src/components/attest/attest.tsx
+++ apps/web/src/components/attest/attest.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useContext, useState } from "react";
+import { useEffect, useContext } from "react";
 import { AttestContext } from "../context/attestContext";
 import { SelectStep } from "./select";
 import { ProofStep } from "./proof";
@@ -19,7 +19,6 @@ async function timeout(seconds: number): Promise<void> {
 
 const Attest = () => {
   const attest = useContext(AttestContext);
-  const [debug, setDebug] = useState<boolean>(false);
 
   useEffect(() => {
     (async () => {
@@ -53,28 +52,6 @@ const Attest = () => {
     })();
   }, []);
 
-  // Import contract
-  // useEffect(() => {
-  //   (async () => {
-  //     console.log("Loading snarkyjs and zkApp");
-  //     const { Mina, PublicKey } = await import("snarkyjs");
-  //     const { Zap } = await import("@contracts/zap/build/src/Zap");
-
-  //     // Update this to use the address (public key) for your zkApp account.
-  //     // To try it out, you can try this address for an example "Add" smart contract that we've deployed to
-  //     // Berkeley Testnet B62qkwohsqTBPsvhYE8cPZSpzJMgoKn4i1LQRuBAtVXWpaT4dgH6WoA.
-  //     const zkAppAddress = process.env["ZK_APP"];
-  //     // This should be removed once the zkAppAddress is updated.
-  //     if (!zkAppAddress) {
-  //       console.error(
-  //         'The following error is caused because the zkAppAddress has an empty string as the public key. Update the zkAppAddress with the public key for your zkApp account, or try this address for an example "Add" smart contract that we deployed to Berkeley Testnet: B62qkwohsqTBPsvhYE8cPZSpzJMgoKn4i1LQRuBAtVXWpaT4dgH6WoA'
-  //       );
-  //     }
-  //     //const zkApp = new Add(PublicKey.fromBase58(zkAppAddress))
-  //     console.log("zkAppAddress", zkAppAddress);
-  //   })();
-  // }, []);
-
   // 1: Connect wallets
   const step1 = !(
     attest.minaWallet.isConnected && attest.ethereumWallet.isConnected
@@ -90,7 +67,7 @@ const Attest = () => {
       {step1 && !step2 && (
         <div className="flex flex-col py-4">
           <h2 className="py-2 text-center inline bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text font-display text-6xl tracking-tight text-transparent">
-            Choose your attestation 
+            Connect your wallets
           </h2>
           <p className="font-light mt-3 text-sm text-center tracking-tight text-slate-400 max-w-xl mx-auto">
             Please connect your wallets! Your ethereum wallet will be the source
@@ -105,15 +82,24 @@ const Attest = () => {
         </div>
       )}
       {step2 && !step3 && (
-        <div className="border-2 border-gray-500 p-4 rounded-xl bg-gray-100">
-          STEP2
+        <div className="flex flex-col py-4">
+          <h2 className="py-2 text-center inline bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text font-display text-6xl tracking-tight text-transparent">
+            Choose your statement 
+          </h2>
+          <p className="font-light mt-3 text-sm text-center tracking-tight text-slate-400 max-w-xl mx-auto">
+            The statement define the condition that will be attested on a specific data source.
+          </p>
           <SelectStep />
         </div>
       )}
       {step3 && (
-        <div className="border-2 border-gray-500 p-4 rounded-xl bg-gray-100">
-          STEP3
-          <p>Generating proof and send tx on Mina ZAP contract!</p>
+        <div className="flex flex-col py-4">
+          <h2 className="py-2 text-center inline bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text font-display text-6xl tracking-tight text-transparent">
+            Generate and submit proof
+          </h2>
+          <p className="font-light mt-3 text-sm text-center tracking-tight text-slate-400 max-w-xl mx-auto">
+            Generate a Zero-Knowledge proof of the statement and submit it to the ZAP protocol on Mina.
+          </p>
           <ProofStep />
         </div>
       )}
