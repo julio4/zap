9729e07fb66dfc93baca23ae6d8ffb1e6c3e3730
julio4 <jules.doumeche@gmail.com>
2023-12-07 19:19:43 +0900

    feat: tests /example/nb endpoint

--- templates/source/src/index.ts
+++ templates/source/src/index.ts
@@ -1,6 +1,10 @@
 import app from "./app";
 import 'dotenv/config'
 
+if (!process.env.PRIVATE_KEY) {
+  throw new Error("Missing PRIVATE_KEY env variable (see .env.example)");
+}
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
