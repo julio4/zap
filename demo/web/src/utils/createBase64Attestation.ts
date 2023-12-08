636b3297691d28621bbd0fa974fd5669cd791d02
julienbrs <julien.bourseau@gmail.com>
2023-12-08 23:57:12 +0100

    verify to prevent scam base64note

--- demo/web/src/utils/createBase64Attestation.ts
+++ demo/web/src/utils/createBase64Attestation.ts
@@ -1,4 +1,4 @@
-import { Condition, AttestationNote, OracleRequest, OracleRoute } from "../types";
+import { Condition, AttestationNote, OracleRequest } from "../types";
 
 const getConditionString = (condition: Condition) => {
   switch (condition) {
@@ -65,21 +65,3 @@ export const decodeAttestationNote = (
 
   return attestation as AttestationNote;
 };
-
-const fakeNote=createAttestationNoteEncoded(
-  Condition.GREATER_THAN,
-  1000,
-  9000,
-  {
-    route: OracleRoute.BALANCE,
-    args: {
-        token: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
-        blockchain: "polygon"
-    }
-},
-  "24771613593952144968161186171837982514830394192519219838611160378286992541903",
-  "17613068260150588986788240600449359622364004875948701399924500458447005281944",
-  'B62qm3bbCSy8ixuacL8FJzWdoj9MBjQGgrzHwiHtksBHTtmFWhidKxS'
-)
-
-console.log("my fake note is: ", fakeNote)
