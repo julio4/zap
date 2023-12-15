9502289545fb25f66690f179a2e21e63ae07c829
julio4 <jules.doumeche@gmail.com>
2023-12-15 21:12:50 +0100

    feat: ts and jest config for o1js/supertest/express

--- templates/source/jest.config.js
+++ templates/source/jest.config.js
@@ -1,11 +1,17 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  roots: ['./src', './tests'],
+  preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  transformIgnorePatterns: ['node_modules/', 'dist/node/'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  moduleNameMapper: {
     axios: 'axios/dist/node/axios.cjs',
+    o1js: 'o1js/dist/node/index.cjs',
   },
-};
+}
