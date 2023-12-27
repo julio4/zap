826ee85ef471c793eae848f2515703abbefe0e9e
julio4 <jules.doumeche@gmail.com>
2023-12-27 11:45:42 +0100

    feat: @packages/zap-utils + tsconfig

---
+++ packages/zap-utils/src/index.ts
@@ -0,0 +1,66 @@
+import {
+  PrivateKey,
+  Signature,
+  PublicKey,
+  Encoding,
+  Field,
+  Poseidon,
+} from "o1js";
+
+import {
+  ZapResponse,
+  ZapHashedResponse,
+  ZapSignedResponse,
+  Route,
+} from "./types.js"
+
+export const signResponse = (
+  res: ZapResponse,
+  privateKey: PrivateKey
+): ZapSignedResponse => {
+  const publicKey = privateKey.toPublicKey().toBase58();
+
+  // Compute the hash of the route and format the response data
+  const data = hashResponse(res);
+
+  const signature = Signature.create(
+    privateKey,
+    encodeResAsFields(data)
+  ).toBase58();
+
+  return {
+    data,
+    signature,
+    publicKey,
+  };
+};
+
+export const verifyResponseSignature = (res: ZapSignedResponse, publicKey: PublicKey): boolean => {
+  const data = encodeResAsFields(res.data);
+  const signature = Signature.fromBase58(res.signature);
+
+  return signature.verify(publicKey, data).toBoolean();
+};
+
+export const hashRoute = (route: Route): Field => {
+  const routeAsFields: Field[] = Encoding.stringToFields(JSON.stringify(route));
+  return Poseidon.hash(routeAsFields);
+};
+
+export const hashResponse = (res: ZapResponse): ZapHashedResponse => ({
+  value: res.value,
+  hashRoute: hashRoute(res.route).toString(),
+});
+
+export const encodeResAsFields = (res: ZapHashedResponse): Field[] => {
+  const data = [res.value, res.hashRoute];
+  return data.map((value) => Field.from(value));
+};
+
+export const fieldsToStrings = (fields: Field[]): string[] => {
+  return fields.map((field) => field.toString());
+};
+
+export const fieldStrToStrings = (field: string): string => {
+  return Field.from(field).toString();
+};
---
+++ packages/zap-utils/src/types.ts
@@ -0,0 +1,50 @@
+export type ZapRequestParams = {
+  mina_address: string;
+  args?: {
+    [key: string]: any;
+  }
+}
+
+export type Route = {
+  path: string;
+  args?: {
+    [key: string]: any;
+  }
+};
+
+/*
+ * Response Object (Unsigned)
+ */
+
+// The response body that each controller will return in the res.json() call
+type SourceResponseBody<T> = {
+  value: T;
+  route: Route;
+};
+// Supported response types
+export type SupportedValue = number;
+export type ZapResponse = SourceResponseBody<SupportedValue>;
+
+// The response body with hashed route
+// @see helpers#hashResponse
+type SourceHashedResponse<T> = {
+  value: T;
+  hashRoute: string; // Hash(Route)
+}
+// Supported response types
+export type ZapHashedResponse = SourceHashedResponse<number>;
+
+/*
+ * Signed Response Object
+ * This is what should be sent and received by the client
+ * The integrity of `data` is ensured by the `signature` and `publicKey`
+ */
+export type SignedResponse<T> = {
+  data: T;
+  signature: string;  // Signature.toBase58()
+  publicKey: string;  // PublicKey.toBase58()
+};
+
+// This is the response that the client will receive
+// In it, data contains the value and the corresponding route (with args)
+export type ZapSignedResponse = SignedResponse<ZapHashedResponse>;
