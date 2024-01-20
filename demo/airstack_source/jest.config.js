0ff90192688a36e82cb0ffe7d1f041749583ec5d
julio4 <jules.doumeche@gmail.com>
2024-01-20 23:53:54 +0900

    feat: automatic endpoints detection with swagger

--- demo/airstack_source/jest.config.js
+++ demo/airstack_source/jest.config.js
@@ -16,7 +16,6 @@ export default {
   },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
-    axios: 'axios/dist/node/axios.cjs',
     o1js: 'o1js/dist/node/index.cjs',
   },
   transform: {},
