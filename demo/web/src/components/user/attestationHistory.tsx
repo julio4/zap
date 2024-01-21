db89bb7480fa29c3861795c7c6d487cee0354eaa
julienbrs <julien.bourseau@gmail.com>
2024-01-21 02:20:49 +0100

    sidebar ok + temp UI done

--- demo/web/src/components/user/attestationHistory.tsx
+++ demo/web/src/components/user/attestationHistory.tsx
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
 import AttestationSidebar from "./attestationSidebar";
+import { AttestationNote } from "../../types";
+
+let attestationExample: AttestationNote = {
+    attestationHash: "0x123456789",
+    statement: "Attestation 1",
+    targetValue: 100,
+    conditionType: "Less Than",
+    hashRoute: "0x123456789",
+    sender: "0x123456789",
+};
 
 interface AttestationHistoryProps {
-    attestations: any[];
+    attestations: AttestationNote[];
     isLoading: boolean;
 }
 
 const AttestationHistory: React.FC<AttestationHistoryProps> = ({ attestations, isLoading }) => {
-    const hardcodedAttestations = ["Attestation 1", "Attestation 2", "Attestation 3", "Attestation 4", "Attestation 5"];
+    const hardcodedAttestations = [attestationExample, attestationExample, attestationExample, attestationExample]
     const [selectedAttestation, setSelectedAttestation] = useState<any>(null);
 
 
@@ -20,26 +30,28 @@ const AttestationHistory: React.FC<AttestationHistoryProps> = ({ attestations, i
     }
 
     return (
-        <div className="flex flex-col py-4 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+
+        <div className={`flex flex-col py-4 max-w-7xl px-4 sm:px-6 lg:px-8 ${selectedAttestation ? 'w-2/5' : 'w-1/2  mx-auto'}`}>
             <h2 className="py-2 text-center inline bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text font-display text-6xl tracking-tight text-transparent">
                 Attestation History
             </h2>
             <p className="font-light mt-3 text-sm text-center tracking-tight text-slate-400 max-w-xl mx-auto">
                 You can see here all the attestations you have made in the past.
             </p>
-
-            <div className="mt-6 w-full grid grid-cols-1 gap-4">
+            <div className="mt-6 grid grid-cols-1 gap-4 ">
                 {hardcodedAttestations.map((attestation, index) => (
                     <div
                         key={index}
-                        className="cursor-pointer ring-slate-200 hover:ring-slate-300 bg-slate-800/75 hover:bg-slate-700/75 px-5 py-3 rounded-xl hover:scale-[1.02] duration-300 ease-in-out transition-all"
+                        className="cursor-pointer ring-slate-200 hover:ring-slate-300 bg-slate-800/75 hover:bg-slate-700/75 px-10 py-3 rounded-xl hover:scale-[1.02] duration-300 ease-in-out transition-all"
                         onClick={() => handleClick(attestation)}
                     >
                         <div className="flex justify-between items-center">
                             <div className="flex items-center">
-                                <h4 className="text-sm font-semibold">{attestation}</h4>
+                                <h4 className="text-sm font-semibold text-slate-100">{attestation.statement}</h4>
                             </div>
-                            <div className="text-sm font-semibold">Date</div>
+                            <div className="flex items-center"> <h4 className="text-sm font-semibold text-slate-100">{attestation.attestationHash}</h4></div>
+                            <div className="text-sm font-semibold text-slate-100">17/06/2023</div>
+
                         </div>
                     </div>
                 ))}
--- demo/web/src/components/user/attestationSidebar.tsx
+++ demo/web/src/components/user/attestationSidebar.tsx
@@ -1,8 +1,50 @@
 import React from "react";
 
-const AttestationSidebar = ({ attestation }) => {
+import { AttestationNote } from "../../types";
+
+const AttestationSidebar = ({ attestation }: { attestation: AttestationNote }) => {
     return (
-        <div className="fixed right-0 top-0 h-full w-1/2 bg-white shadow-lg">        </div>
+        <div className="fixed right-0 h-3/4 w-1/2 bg-slate-700/40 shadow-lg z-99 my-auto mr-4 rounded-xl overflow-y-auto">
+            <div className="flex flex-col py-12 px-8">
+                <div className="text-center mb-8">
+                    <h3 className="py-2 inline bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">
+                        Attestation Details
+                    </h3>
+                </div>
+
+                <div className="px-4">
+                    <div className="mb-4">
+                        <h4 className="text-xl text-white">Statement:</h4>
+                        <p className="text-lg text-gray-300">{attestation.statement}</p>
+                    </div>
+                    <div className="mb-4">
+                        <h4 className="text-xl text-white">Attestation Hash:</h4>
+                        <p className="text-lg text-gray-300">{attestation.attestationHash}</p>
+                    </div>
+                    <hr className="border-t border-gray-600" />
+
+                    <div className="my-4">
+                        <h4 className="text-xl text-white">Hash Route:</h4>
+                        <p className="text-lg text-gray-300">{attestation.hashRoute}</p>
+                    </div>
+                    <hr className="border-t border-gray-600" />
+                    <div className="my-4">
+                        <h4 className="text-xl text-white">Sender:</h4>
+                        <p className="text-lg text-gray-300">{attestation.sender}</p>
+                    </div>
+                    <hr className="border-t border-gray-600" />
+                    <div className="my-4">
+                        <h4 className="text-xl text-white">Target Value:</h4>
+                        <p className="text-lg text-gray-300">{attestation.targetValue}</p>
+                    </div>
+                    <hr className="border-t border-gray-600" />
+                    <div className="my-4">
+                        <h4 className="text-xl text-white">Condition Type:</h4>
+                        <p className="text-lg text-gray-300">{attestation.conditionType}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
