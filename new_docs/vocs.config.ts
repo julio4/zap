2ba56eae74ce257f4ffc0d856926ba0d135008e8
julienbrs <julien.bourseau@gmail.com>
2024-01-25 21:27:11 +0100

    Init vocs

---
+++ new_docs/vocs.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vocs'
+ 
+export default defineConfig({
+  description: 'Build reliable apps & libraries with lightweight, \
+    composable, and type-safe modules that interface with Ethereum.', 
+  title: 'Viem',
+  rootDir: '.',
+})
