b30bad7233b06e5bffdac8827e7b49869816a39c
julienbrs <julien.bourseau@gmail.com>
2024-01-25 22:39:44 +0100

    adding sidebar

--- new_docs/vocs.config.ts
+++ new_docs/vocs.config.ts
@@ -1,8 +1,86 @@
 import { defineConfig } from 'vocs'
- 
+
 export default defineConfig({
-  description: 'Build reliable apps & libraries with lightweight, \
-    composable, and type-safe modules that interface with Ethereum.', 
-  title: 'Viem',
+
+  titleTemplate: '%s – ZAP',
+  logoUrl: '⚡',
+  iconUrl: '⚡',
+  editLink: {
+    pattern: 'https://github.com/julio4/zap/edit/main/docs/:path',
+    text: 'Edit on GitHub'
+  },
+
+  // TODO: footer
+  // todo: sidebar
+  // todo: navbar
+  sidebar: [ 
+    { 
+        text: 'What are (Zk)Oracles?', 
+        link: '/intro', 
+    }, 
+    { 
+        text: 'ZAP Overview', 
+        link: '/', 
+    }, 
+    { 
+        text: 'Project Milestones', 
+        link: '/milestone', 
+    },
+    { 
+        text: 'Protocol Concepts', 
+        collapsed: true, 
+        items: [ 
+            { 
+            text: 'Statement', 
+            link: 'concepts/Statement', 
+            }, 
+            {
+                text: 'Attestation',
+                link: 'concepts/Attestation',
+            },
+            {
+                text: 'ZAP Source',
+                link: 'concepts/ZAPSource',
+            }
+        ],
+    },
+    {
+        text: 'ZAP Protocol', 
+        link: '/protocol',
+    },
+    { 
+        text: 'Technical Reference', 
+        collapsed: true, 
+        items: [ 
+            { 
+            text: 'ZAP Contract', 
+            link: 'reference/ZapContract', 
+            }, 
+        ],
+    },
+    { 
+        text: 'Guides', 
+        collapsed: true, 
+        items: [ 
+            { 
+            text: 'User guide', 
+            link: 'guides/user', 
+            }, 
+            {
+                text: 'Test ZAP Locally',
+                link: 'guides/localenv',
+            }
+        ],
+    },
+  ],
+
+  theme: {
+    // accentColor: '#our-accent-color-is ????',
+    variables: {
+    },
+  },
+
   rootDir: '.',
+  description: 'The first Zero-Knowledge Attestation Protocol leveraging zk-SNARKs on Mina Protocol',
+  title: 'ZAP',
 })
