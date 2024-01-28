3ea8b573643ab58d6eb9f7e59181c5066afd6e15
julio4 <jules.doumeche@gmail.com>
2024-01-28 19:24:33 +0900

    feat: @template/ui

---
+++ templates/ui/src/pages/index.tsx
@@ -0,0 +1,46 @@
+import Head from "next/head";
+import { useEffect } from "react";
+import GradientBG from "../components/GradientBG.js";
+import styles from "../styles/Home.module.css";
+
+import { ZapRequestParams } from "@zap/utils";
+import { Route } from "@zap/utils/source";
+
+const loadZap = async () => {
+  const { Mina, PublicKey } = await import("o1js");
+  const { Zap } = await import("@zap/core");
+
+  const zkAppAddress =
+    "B62qnhBxxQr7h2AE9f912AyvzJwK1fhEJq7NMZXbzXbhoepUZ7z7237";
+  // TODO remove
+  if (!zkAppAddress) {
+    console.error(
+      'The following error is caused because the zkAppAddress has an empty string as the public key. Update the zkAppAddress with the public key for your zkApp account, or try this address for an example "Add" smart contract that we deployed to Berkeley Testnet: B62qkwohsqTBPsvhYE8cPZSpzJMgoKn4i1LQRuBAtVXWpaT4dgH6WoA'
+    );
+  }
+  const zkApp = new Zap(PublicKey.fromBase58(zkAppAddress));
+  console.log("zkApp", zkApp);
+};
+
+export default function Home(): JSX.Element {
+  useEffect(() => {
+    loadZap();
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Zap zkApp UI</title>
+        <meta name="description" content="built with o1js" />
+        <link rel="icon" href="/assets/favicon.ico" />
+      </Head>
+      <GradientBG>
+        <main className={styles.main}>
+          <p className={styles.start}>
+            Minimal Next.js Mina Zap template
+          </p>
+        </main>
+      </GradientBG>
+    </>
+  );
+}
