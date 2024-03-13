9117dd10981ede5647873fc5db9cde954244b458
julienbrs <julien.bourseau@gmail.com>
2024-03-13 20:48:45 +0100

    feat: test Source

---
+++ packages/core/src/registry/Source.test.ts
@@ -0,0 +1,21 @@
+import { Mina, PrivateKey, PublicKey, AccountUpdate, Poseidon } from 'o1js';
+import { Source } from './Source';
+import { stringToFields } from 'o1js/dist/node/bindings/lib/encoding';
+
+
+describe('Source', () => {
+
+  let source: Source = Source.from(
+    PrivateKey.random().toPublicKey(),
+    stringToFields('http://test.com')[0],
+    stringToFields('name')[0],
+    stringToFields('description')[0],
+  );
+
+  it('hashes the source', () => {
+    const expectedHash = Poseidon.hash([Poseidon.hash(source.publicKey.toFields()), source.urlApi, source.name, source.description]);
+    const hash = source.hash();
+    expect(hash).toBeDefined();
+    expect(hash).toEqual(expectedHash);
+  });
+});
