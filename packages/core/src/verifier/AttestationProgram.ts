09686ed38ef3857a3d34504513b07524ab543bbb
julienbrs <julien.bourseau@gmail.com>
2024-03-15 15:10:32 +0100

    feat: First draft zkProgram recursive proof attestation

---
+++ packages/core/src/verifier/AttestationProgram.ts
@@ -0,0 +1,38 @@
+import { SelfProof, Field, ZkProgram, verify, Struct, Signature } from 'o1js';
+import { ProvableStatement } from '../Statement';
+
+const AttestationProgram = ZkProgram({
+  name: 'AttestationAggregator',
+
+  methods: {
+    baseCase: {
+      privateInputs: [ProvableStatement, Field, Signature],
+
+      method(
+        statement: ProvableStatement,
+        privateData: Field,
+        signature: Signature
+      ) {
+        statement.assertValidSignature(privateData, signature);
+        statement.assertValidCondition(privateData);
+      },
+    },
+
+    step: {
+      privateInputs: [ProvableStatement, Field, Signature, SelfProof],
+      method(
+        statement: ProvableStatement,
+        privateData: Field,
+        signature: Signature,
+        earlierProof: SelfProof<[ProvableStatement, Field, Signature], void>
+      ) {
+        earlierProof.verify();
+        statement.assertValidCondition(privateData);
+      },
+    },
+  },
+});
+
+export class AttestationProgramProof extends ZkProgram.Proof(
+  AttestationProgram
+) {}
