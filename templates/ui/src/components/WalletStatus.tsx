ba6974e8e6cc64eb9de890225f0724fc8e1e675f
julienbrs <julien.bourseau@gmail.com>
2024-03-19 21:04:52 +0100

    ux: mina wallet update detected in real time

--- templates/ui/src/components/WalletStatus.tsx
+++ templates/ui/src/components/WalletStatus.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useZapStore from '../store/zapStore';
 
 type WalletStatusProps = {
   hasWallet: boolean | null;
   publicKey: string;
 };
 
-const WalletStatus = ({ hasWallet, publicKey }: WalletStatusProps) => {
+const WalletStatus = () => {
+  const { hasWallet, publicKey, setZapState } = useZapStore(state => ({
+    hasWallet: state.zapState.hasWallet,
+    publicKey: state.zapState.publicKey?.toBase58() || "",
+    setZapState: state.setZapState,
+  }));
+
+  useEffect(() => {
+    const mina = (window as any).mina;
+    if (mina == null) {
+      setZapState({ hasWallet: false });
+    }
+  }, [setZapState]);
+
   if (hasWallet === false) {
     return (
       <div>Could not find a wallet. <a href="https://www.aurowallet.com/" target="_blank" rel="noreferrer">Install Auro wallet here</a></div>
