69af75990a69045a57089ebb2c1ed0b1eeee95bf
julienbrs <julien.bourseau@gmail.com>
2024-03-20 11:11:33 +0100

    feat: adapt zapstore to new types and use

--- templates/ui/src/store/zapStore.tsx
+++ templates/ui/src/store/zapStore.tsx
@@ -1,39 +1,17 @@
 import { create } from 'zustand';
-import { ZapWorkerClient } from '@zap/client';
-import { PublicKey } from 'o1js';
+import { ZapState, ZapActions, ZapStore } from '../types';
 
-type ZapState = {
-    zapWorkerClient: ZapWorkerClient | null;
-    hasWallet: boolean | null;
-    hasBeenSetup: boolean;
-    accountExists: boolean;
-    publicKey: PublicKey | null;
-    zapPublicKey: PublicKey | null;
-    creatingTransaction: boolean;
-};
-
-interface ZapActions {
-    setZapState: (newState: Partial<ZapState>) => void;
-}
-
-type ZapStore = {
-    zapState: ZapState;
-} & ZapActions;
-
-const useZapStore: () => ZapStore
-    = create<ZapStore>((set) => ({
-        zapState: {
-            zapWorkerClient: null,
-            hasWallet: null,
-            hasBeenSetup: false,
-            accountExists: false,
-            publicKey: null,
-            zapPublicKey: null,
-            creatingTransaction: false,
-        },
-        setZapState: (newState) => set((state) => ({
-            zapState: { ...state.zapState, ...newState },
-        })),
-    }));
+const useZapStore = create<ZapStore>((set) => ({
+    zapState: {
+        zapWorkerClient: null,
+        hasWallet: null,
+        hasBeenSetup: false,
+        accountExists: false,
+        publicKey: null,
+        zapPublicKey: null,
+        creatingTransaction: false,
+    },
+    setZapState: (newState: Partial<ZapState>) => set(({ zapState }) => ({ zapState: { ...zapState, ...newState } })),
+}));
 
 export default useZapStore;
