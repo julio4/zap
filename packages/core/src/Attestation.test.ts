c53e1394563f599723b15a945a086aa24cc3da81
julio4 <jules.doumeche@gmail.com>
2024-04-05 21:53:23 +0900

    test: coverage for Attestation

--- packages/core/src/Attestation.test.ts
+++ packages/core/src/Attestation.test.ts
@@ -1,10 +1,11 @@
-import { Statement } from '@zap/types';
+import { Statement, ConditionType } from '@zap/types';
 import { Attestation } from './Attestation';
 import { ProvableStatement } from './Statement';
-import { Field, Poseidon, PrivateKey } from 'o1js';
+import { Bool, Field, Poseidon, PrivateKey, Sign, Signature } from 'o1js';
 
 describe('Attestation', () => {
-  const sourceKey = PrivateKey.random().toPublicKey();
+  const sourcePrivateKey = PrivateKey.random();
+  const sourceKey = sourcePrivateKey.toPublicKey();
   const address = PrivateKey.random().toPublicKey();
   const statement: Statement = {
     sourceKey: sourceKey.toBase58(),
@@ -13,25 +14,33 @@ describe('Attestation', () => {
       args: {},
     },
     condition: {
-      type: 1,
+      type: ConditionType.EQ,
       targetValue: 1,
     },
   };
+  const privateData = Field(1);
+
   const attestation = new Attestation({
     statement: ProvableStatement.from(statement),
+    privateData,
+    signature: ProvableStatement.sign(statement, privateData, sourcePrivateKey),
     address,
   });
 
-  it('address', () => {
+  it('Attestation.address', () => {
     expect(attestation.address).toEqual(address);
   });
 
-  it('statement', () => {
+  it('Attestation.statement', () => {
     const provableStatement = ProvableStatement.from(statement);
     expect(attestation.statement).toEqual(provableStatement);
   });
 
-  it('hash()', () => {
+  it('Attestation.privateData', () => {
+    expect(attestation.privateData).toEqual(privateData);
+  });
+
+  it('Attestation.hash()', () => {
     const { hashRoute, conditionType, targetValue, source } =
       attestation.statement;
     const message = [
@@ -45,11 +54,233 @@ describe('Attestation', () => {
     expect(attestation.hash()).toEqual(digest);
   });
 
-  it('assertEqual()', () => {
+  it('Attestation.assertEqual()', () => {
     const hash = attestation.hash();
     expect(() => attestation.assertEqual(hash)).not.toThrow();
     expect(() =>
       attestation.assertEqual(Poseidon.hash([Field(0x1)]))
     ).toThrow();
+  });
+
+  it('Attestation.assertValidSignature()', () => {
+    expect(() => attestation.assertValidSignature()).not.toThrow();
+
+    expect(() =>
+      new Attestation({
+        statement: ProvableStatement.from(statement),
+        privateData: Field(0),
+        signature: ProvableStatement.sign(
+          statement,
+          privateData,
+          sourcePrivateKey
+        ),
+        address,
+      }).assertValidSignature()
+    ).toThrow();
+
+    expect(() =>
+      new Attestation({
+        statement: ProvableStatement.from(statement),
+        privateData,
+        signature: ProvableStatement.sign(
+          statement,
+          privateData,
+          PrivateKey.random()
+        ),
+        address,
+      }).assertValidSignature()
+    ).toThrow();
+
+    expect(() =>
+      new Attestation({
+        statement: ProvableStatement.from(statement),
+        privateData,
+        signature: Signature.create(sourcePrivateKey, [privateData, Field(0)]),
+        address,
+      }).assertValidSignature()
+    ).toThrow();
+
+    expect(() =>
+      new Attestation({
+        statement: ProvableStatement.from(statement),
+        privateData,
+        signature: Signature.create(sourcePrivateKey, [
+          Field(0),
+          attestation.statement.hashRoute,
+        ]),
+        address,
+      }).assertValidSignature()
+    ).toThrow();
+  });
+
+  it('Attestation.isValidSignature()', () => {
+    expect(attestation.isValidSignature()).toEqual(Bool(true));
+
+    expect(
+      new Attestation({
+        statement: ProvableStatement.from(statement),
+        privateData: Field(0),
+        signature: ProvableStatement.sign(
+          statement,
+          privateData,
+          sourcePrivateKey
+        ),
+        address,
+      }).isValidSignature()
+    ).toEqual(Bool(false));
+
+    expect(
+      new Attestation({
+        statement: ProvableStatement.from(statement),
+        privateData,
+        signature: ProvableStatement.sign(
+          statement,
+          privateData,
+          PrivateKey.random()
+        ),
+        address,
+      }).isValidSignature()
+    ).toEqual(Bool(false));
+
+    expect(
+      new Attestation({
+        statement: ProvableStatement.from(statement),
+        privateData,
+        signature: Signature.create(sourcePrivateKey, [privateData, Field(0)]),
+        address,
+      }).isValidSignature()
+    ).toEqual(Bool(false));
+
+    expect(
+      new Attestation({
+        statement: ProvableStatement.from(statement),
+        privateData,
+        signature: Signature.create(sourcePrivateKey, [
+          Field(0),
+          attestation.statement.hashRoute,
+        ]),
+        address,
+      }).isValidSignature()
+    ).toEqual(Bool(false));
+  });
+
+  it('Attestation.assertValidCondition()', () => {
+    expect(() => attestation.assertValidCondition()).not.toThrow();
+
+    expect(() =>
+      new Attestation({
+        statement: ProvableStatement.from(statement),
+        privateData: Field(0),
+        signature: ProvableStatement.sign(
+          statement,
+          Field(0),
+          sourcePrivateKey
+        ),
+        address,
+      }).assertValidCondition()
+    ).toThrow();
+
+    expect(() =>
+      new Attestation({
+        statement: ProvableStatement.from(statement),
+        privateData: Field(2),
+        signature: ProvableStatement.sign(
+          statement,
+          Field(2),
+          sourcePrivateKey
+        ),
+        address,
+      }).assertValidCondition()
+    ).toThrow();
+  });
+
+  it('Attestation.isValidCondition()', () => {
+    expect(attestation.isValidCondition()).toEqual(Bool(true));
+
+    expect(
+      new Attestation({
+        statement: ProvableStatement.from(statement),
+        privateData: Field(0),
+        signature: ProvableStatement.sign(
+          statement,
+          Field(0),
+          sourcePrivateKey
+        ),
+        address,
+      }).isValidCondition()
+    ).toEqual(Bool(false));
+
+    expect(
+      new Attestation({
+        statement: ProvableStatement.from(statement),
+        privateData: Field(2),
+        signature: ProvableStatement.sign(
+          statement,
+          Field(2),
+          sourcePrivateKey
+        ),
+        address,
+      }).isValidCondition()
+    ).toEqual(Bool(false));
+  });
+
+  it('Attestation.assertValid()', () => {
+    expect(() => attestation.assertValid()).not.toThrow();
+
+    expect(() =>
+      new Attestation({
+        statement: ProvableStatement.from(statement),
+        privateData: Field(0),
+        signature: ProvableStatement.sign(
+          statement,
+          Field(0),
+          sourcePrivateKey
+        ),
+        address,
+      }).assertValid()
+    ).toThrow();
+
+    expect(() =>
+      new Attestation({
+        statement: ProvableStatement.from(statement),
+        privateData: Field(2),
+        signature: ProvableStatement.sign(
+          statement,
+          Field(2),
+          sourcePrivateKey
+        ),
+        address,
+      }).assertValid()
+    ).toThrow();
+  });
+
+  it('Attestation.isValid()', () => {
+    expect(attestation.isValid()).toEqual(Bool(true));
+
+    expect(
+      new Attestation({
+        statement: ProvableStatement.from(statement),
+        privateData: Field(0),
+        signature: ProvableStatement.sign(
+          statement,
+          Field(0),
+          sourcePrivateKey
+        ),
+        address,
+      }).isValid()
+    ).toEqual(Bool(false));
+
+    expect(
+      new Attestation({
+        statement: ProvableStatement.from(statement),
+        privateData: Field(2),
+        signature: ProvableStatement.sign(
+          statement,
+          Field(2),
+          sourcePrivateKey
+        ),
+        address,
+      }).isValid()
+    ).toEqual(Bool(false));
   });
 });
