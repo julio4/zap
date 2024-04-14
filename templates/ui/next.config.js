0e512c87a71e0d2eed46af96cf0bb30479b2339d
julio4 <jules.doumeche@gmail.com>
2024-04-14 17:26:32 +0900

    ui: fix tailwindcss config

--- templates/ui/next.config.js
+++ templates/ui/next.config.js
@@ -2,11 +2,10 @@
 const nextConfig = {
   reactStrictMode: false,
 
-
   webpack(config) {
     config.resolve.alias = {
       ...config.resolve.alias,
-      o1js: require('path').resolve('node_modules/o1js')
+      o1js: require("path").resolve("node_modules/o1js"),
     };
     config.experiments = { ...config.experiments, topLevelAwait: true };
     return config;
@@ -16,20 +15,20 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: "/(.*)",
         headers: [
           {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
           },
           {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
           },
         ],
       },
     ];
-  }
+  },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
