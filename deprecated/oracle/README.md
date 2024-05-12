753032fe220cfa4fb652f61cdd1ca9a65e76000c
julio4 <jules.doumeche@gmail.com>
2024-05-12 15:13:44 +0900

    chore: cleaning

--- deprecated/oracle/README.md
+++
@@ -1,3 +0,0 @@
-# ZAP Airstack Oracle
-
-This is a simple oracle server that can be used to get signed data from ethereum by using Airstack's graphql API.
--- deprecated/oracle/src/airstack/config.ts
+++
@@ -1,8 +0,0 @@
-const AIRSTACK_ENDPOINT =
-  process.env['AIRSTACK_ENDPOINT'] || 'https://api.airstack.xyz/gql/';
-
-const defaultBlockchain = 'ethereum';
-
-// pagination limits, API keys, etc.
-
-export { AIRSTACK_ENDPOINT, defaultBlockchain };
